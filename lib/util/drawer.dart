9794aca6b801f937cb20f745c0dde4c60f447f8a
saeed <134363573+Saeeeddev@users.noreply.github.com>
2023-11-16 20:47:26 +0330

    Create drawer.dart

---
+++ lib/util/drawer.dart
@@ -0,0 +1,29 @@
+import:'package:flutter/materail.dart';
+
+class Mydrawer extends StatelessWidget {
+  const Mydrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.red,
+        width: 200,
+        child: Column(
+          children: [
+            DrawerHeader(
+                child: Text(
+              'M E N U'
+              style: TextStyle(fontSize: 20, fontFamily: 'Freedom'),
+            )),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('H O M E'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ), 
+    );
+  }
+}
